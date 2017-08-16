4daafaf0d1462552c9f612605f09830ed1d89c68
Philip Meier <philip.meier06@googlemail.com>
2017-08-16 17:10:13 +0100

    Add post /game route updating game state
    This is dumb working version with everything in the sinatra code.
    Next step will be to push out the various bits of logic under test.

--- ttt_web.rb
+++ ttt_web.rb
@@ -1,11 +1,31 @@
 require 'sinatra/base'
 require 'ttt_core'
 
+class Player
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+end
+
 class Application < Sinatra::Base
-  
+  set :sessions, true
+
   get '/game' do
     board = TttCore::Board.new
+    player_one = Player.new("X")
+    player_two = Player.new("O")
+    game = TttCore::Game.new(board, player_one, player_two)
+    session[:game] = game
     erb :game, :locals => { :board => board }
+  end
+
+  post '/game' do
+    move = params["move"].to_i
+    game = session[:game]
+    game.player_chooses(move)
+    erb :game, :locals => { :board => game.board }
   end
 
   run! if app_file == $0
