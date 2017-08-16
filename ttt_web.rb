4fb5be1db8d5efa27c6da6916189c4d17a7b51d8
Philip Meier <philip.meier06@googlemail.com>
2017-08-16 17:25:10 +0100

    Push web based player into separate file

--- ttt_web.rb
+++ ttt_web.rb
@@ -1,21 +1,14 @@
 require 'sinatra/base'
 require 'ttt_core'
-
-class Player
-  attr_reader :mark
-
-  def initialize(mark)
-    @mark = mark
-  end
-end
+require_relative 'lib/web_player'
 
 class Application < Sinatra::Base
   set :sessions, true
 
   get '/game' do
     board = TttCore::Board.new
-    player_one = Player.new("X")
-    player_two = Player.new("O")
+    player_one = WebPlayer.new("X")
+    player_two = WebPlayer.new("O")
     game = TttCore::Game.new(board, player_one, player_two)
     session[:game] = game
     erb :game, :locals => { :board => board }
