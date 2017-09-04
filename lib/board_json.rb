9d7afc6d101c2617f77d73a7403b97a8b91df995
Philip Meier <philip.meier06@googlemail.com>
2017-09-04 15:07:47 +0100

    Add api route for result information

--- lib/board_json.rb
+++ lib/board_json.rb
@@ -1,9 +1,19 @@
 require 'json'
+require_relative 'board_spaces'
 
 class BoardJson
   def self.encode(board)
     marks = []
     (1..board.size).each {|space| marks << board.get_mark(space)}
-    JSON.generate({:dimension => board.dimension, :marks => marks, :game_over => board.game_over?})
+    JSON.generate({:dimension => board.dimension,
+                   :marks => marks,
+                   :game_over => board.game_over?})
+  end
+
+  def self.encode_result(board)
+    board_spaces = BoardSpaces.new(board)
+    JSON.generate({:drawn => board.drawn?,
+                   :winning_mark => board.winning_mark,
+                   :winning_spaces => board_spaces.winning_spaces})
   end
 end
